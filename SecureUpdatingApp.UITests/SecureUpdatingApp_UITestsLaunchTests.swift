b931ea326f7c6c7de273731ca11803baf3e33ee5
Lindokuhle Khumalo <klindokuhle1@gmail.com>
2025-03-25 15:26:31 +0200

    Initial Commit

---
+++ SecureUpdatingApp.UITests/SecureUpdatingApp_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SecureUpdatingApp_UITestsLaunchTests.swift
+//  SecureUpdatingApp.UITests
+//
+//  Created by Lindokuhle Khumalo on 2025/03/24.
+//
+
+import XCTest
+
+final class SecureUpdatingApp_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
