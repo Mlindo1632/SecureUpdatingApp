37c627c9f6a67ddb4e1e8b0cd25d64afae9c1822
Lindokuhle Khumalo <klindokuhle1@gmail.com>
2025-03-26 15:38:37 +0200

    Created the NetworkManager and Protocols

---
+++ SecureUpdatingApp./NetworkManager/NetworkManager.swift
@@ -0,0 +1,63 @@
+//
+//  NetworkManager.swift
+//  SecureUpdatingApp.
+//
+//  Created by Lindokuhle Khumalo on 2025/03/26.
+//
+
+import Foundation
+
+class NetworkManager: NetworkManagerProtocol {
+    static let shared = NetworkManager()
+    
+    weak var delegate: NetworkManagerDelegate?
+    
+    private init() {}
+    
+    func request<T: Codable>(
+        endpoint: String,
+        method: HTTPMethod,
+        parameters: [String : Any]?,
+        headers: [String : String]?,
+        completion: @escaping (Result<T, APIError>) -> Void
+    ) {
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method.rawValue
+        if let headers = headers {
+            for (key, value) in headers {
+                request.addValue(value, forHTTPHeaderField: key)
+            }
+        }
+        if let parameters = parameters {
+            request.httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: [])
+        }
+        
+        request.setValue("application/json", forHTTPHeaderField: "content-Type")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let _ = error {
+                completion(.failure(.requestFailed))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.unknownError))
+                return
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(decodedData))
+                self.delegate?.didDecodeData(decodedData)
+            } catch {
+                completion(.failure(.decodingFailed))
+                self.delegate?.didFail(.decodingFailed)
+            }
+        }.resume()
+    }
+}
