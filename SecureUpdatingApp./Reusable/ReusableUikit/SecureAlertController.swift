ed16164f7d876963dec519dc743fa67ee91e0baf
Lindokuhle Khumalo <klindokuhle1@gmail.com>
2025-03-26 22:15:06 +0200

    LoginViewModel fix Created the Following: - LoginView and LoginViewController - Reusable SecureUikit components

---
+++ SecureUpdatingApp./Reusable/ReusableUikit/SecureAlertController.swift
@@ -0,0 +1,20 @@
+//
+//  SecureAlertController.swift
+//  SecureUpdatingApp.
+//
+//  Created by Lindokuhle Khumalo on 2025/03/26.
+//
+
+import Foundation
+import UIKit
+
+struct SecureAlertController {
+    static func showAlert(on viewcontroller: UIViewController, message: String?, title: String?) {
+        DispatchQueue.main.async {
+            let alertController = UIAlertController(title: "Warning", message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: title, style: .default, handler: nil)
+            alertController.addAction(okAction)
+            viewcontroller.present(alertController, animated: true)
+        }
+    }
+}
